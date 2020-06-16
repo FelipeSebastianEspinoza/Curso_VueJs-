7b1d52d7f52543fb5003f493162e4c00f4ee608d
Usuario <felipe@ubuntu.ubuntu-domain>
2020-06-16 19:46:33 -0400

    usando array y funciones de suamr los elementos + un foreach

--- js/vue.js
+++ js/vue.js
@@ -6,18 +6,22 @@ const con = new Vue({
       {
         nombre: "Javascript",
         link: "https://www.javascript.com",
+        puntos: 10,
       },
       {
         nombre: "Python",
         link: "http://php.net",
+        puntos: 5,
       },
       {
         nombre: "Php",
         link: "http://www.python.org",
+        puntos: 1,
       },
     ],
     nuevoLenguaje: "",
     nuevoLink: "",
+    total: 0,
   },
   methods: {
     agregarLenguaje() {
@@ -25,6 +29,18 @@ const con = new Vue({
         nombre: this.nuevoLenguaje,
         link: this.nuevoLink,
       });
+      this.nuevoLenguaje = "";
+      this.nuevoLink = "";
+      e.preventDefault(); // To prevent the form from submitting
+    },
+  },
+  computed: {
+    sumarPuntaje() {
+      this.total = 0;
+      this.lenguajes.forEach((element) => {
+        this.total = this.total + element.puntos;
+      });
+      return this.total;
     },
   },
 });
