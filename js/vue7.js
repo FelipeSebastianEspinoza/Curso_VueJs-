84e757d14663987cc8e9d9229bdf39463b5a42b8
Usuario <felipe@ubuntu.ubuntu-domain>
2020-06-17 18:22:31 -0400

    vuex pasar parametros, sumar restar, pasar al hijo al padre

--- js/vue7.js
+++ js/vue7.js
@@ -1,17 +1,63 @@
 //Padre
 Vue.component("padre", {
   template: `
- 
+    <div class="p-5 bg-dark text-white">
+      <button @click="suma">+</button>
+      <button @click="resta(2)">-</button>
+      <h2>número {{numero}} </h2>
+      <h2>edad {{edad}} </h2>
+      <hijo></hijo>
+    </div>
   `,
+  computed: {
+    ...Vuex.mapState(["edad", "numero"]), //esto hace le código comentado
+
+    /*
+    numero() {
+      return store.state.numero;
+    },
+    edad() {
+      return store.state.edad;
+    },
+    */
+  },
+  methods: {
+    ...Vuex.mapMutations(["suma", "resta"]),
+  },
 });
 //Hijo
 Vue.component("hijo", {
   template: `
-   
+    <div class="p-5 bg-info text-white"> 
+      <button @click="suma">+</button>
+      <button @click="resta(1)">-</button>
+      <h2> número {{$store.state.numero}} </h2>
+    </div>
   `,
+  computed: {
+    ...Vuex.mapState(["edad", "numero"]),
+  },
+  methods: {
+    ...Vuex.mapMutations(["suma", "resta"]),
+  },
+});
+
+const store = new Vuex.Store({
+  state: {
+    numero: 5,
+    edad: 9,
+  },
+  mutations: {
+    suma(state) {
+      state.numero++;
+    },
+    resta(state,num) {
+      state.numero=state.numero-num;
+    },
+  },
 });
 
 const con = new Vue({
   el: "#elemento",
-  data: {},
+  store: store,
 });
