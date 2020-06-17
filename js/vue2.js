ffe694ccc6779468fc91d94dc0d06c4b39d6bf43
Usuario <felipe@ubuntu.ubuntu-domain>
2020-06-16 23:14:54 -0400

    boton cambia color, se usaron consultas if y se pasaraon variables

--- js/vue2.js
+++ js/vue2.js
@@ -1,5 +1,18 @@
 const con = new Vue({
   el: "#elemento",
-  data: {},
-  methods: {},
+  data: {
+    colorFondo: "bg-success",
+    padding: "p-2",
+    colorTexto: "text-white",
+    cambioColor: true,
+  },
+  methods: {
+    cambioColorFunc() {
+      if ((this.cambioColor == false)) {
+        this.cambioColor = true;
+      } else {
+        this.cambioColor = false;
+      }
+    },
+  },
 });
