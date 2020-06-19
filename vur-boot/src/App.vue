7bb0f4f6ed1ffd29f4b1ec5d6c1239b687ab8647
Usuario <felipe@ubuntu.ubuntu-domain>
2020-06-19 10:54:39 -0400

    grid

--- vur-boot/src/App.vue
+++ vur-boot/src/App.vue
@@ -8,10 +8,13 @@
           <b-navbar-nav>
             <b-nav-item :to="{ name: 'Home' }">Home</b-nav-item>
             <b-nav-item :to="{ name: 'About' }">About</b-nav-item>
+            <b-nav-item :to="{ name: 'Grid' }">Grid</b-nav-item>
           </b-navbar-nav>
         </b-collapse>
       </b-container>
     </b-navbar>
-    <router-view />
+    <b-container>
+      <router-view />
+    </b-container>
   </div>
 </template>
---
+++ vur-boot/src/views/Grid.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="mt-5">
+    <b-row>
+      <b-col cols="12" md="6" lg="4">
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Saepe iure
+          quis dolorem laudantium ipsum corrupti voluptates, est facilis.
+          Ratione sint reiciendis voluptatum ex vitae omnis harum odio quasi
+          fugit tempora!
+        </p>
+      </b-col>
+      <b-col cols="12" md="6" lg="8">
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Saepe iure
+          quis dolorem laudantium ipsum corrupti voluptates, est facilis.
+          Ratione sint reiciendis voluptatum ex vitae omnis harum odio quasi
+          fugit tempora!
+        </p>
+      </b-col>
+    </b-row>
+    <b-row>
+      <b-col cols="12" md="6" lg="4">
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Saepe iure
+          quis dolorem laudantium ipsum corrupti voluptates, est facilis.
+          Ratione sint reiciendis voluptatum ex vitae omnis harum odio quasi
+          fugit tempora!
+        </p>
+      </b-col>
+      <b-col cols="12" md="6" lg="8">
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Saepe iure
+          quis dolorem laudantium ipsum corrupti voluptates, est facilis.
+          Ratione sint reiciendis voluptatum ex vitae omnis harum odio quasi
+          fugit tempora!
+        </p>
+      </b-col>
+    </b-row>
+  </div>
+</template>
