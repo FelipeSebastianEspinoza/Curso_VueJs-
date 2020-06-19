82a06102a6fd08d5cb5ed420b48a5f3dcf871575
Usuario <felipe@ubuntu.ubuntu-domain>
2020-06-19 10:17:22 -0400

    vue bootstrap routes

---
+++ vur-boot/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <b-navbar toggleable="md sm" type="dark" variant="info">
+      <b-container>
+        <b-navbar-brand href="#">Titulo</b-navbar-brand>
+        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+        <b-collapse id="nav-collapse" is-nav>
+          <b-navbar-nav>
+            <b-nav-item :to="{ name: 'Home' }">Home</b-nav-item>
+            <b-nav-item :to="{ name: 'About' }">About</b-nav-item>
+          </b-navbar-nav>
+        </b-collapse>
+      </b-container>
+    </b-navbar>
+    <router-view />
+  </div>
+</template>
